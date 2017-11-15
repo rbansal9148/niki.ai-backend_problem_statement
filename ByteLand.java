db782b8c2ce56d0aa298d9b7738a6acb01fdf031
Rubal Bansal <rbansal.rbansal@gmail.com>
2017-11-15 17:17:30 +0530

    Added Solution with Dynamic Programming

--- ByteLand.java
+++ ByteLand.java
@@ -4,18 +4,41 @@ import java.io.IOException;
 
 public class ByteLand{
 
-	public static long calcValidString(int n, boolean isPrevZero){
-		// Exponential Solution
-		if(n==0){
-			return 1;
-		}
-		int sum = 0;
-		if(isPrevZero == false){
-			sum += calcValidString(n-1, true);
-		}
-		sum += calcValidString(n-1, false);
+	// public static long calcValidString(int n, boolean isPrevZero){
+	// 	// Exponential Solution
+	// 	if(n==0){
+	// 		return 1;
+	// 	}
+	// 	int sum = 0;
+	// 	if(isPrevZero == false){
+	// 		sum += calcValidString(n-1, true);
+	// 	}
+	// 	sum += calcValidString(n-1, false);
+	//
+	// 	return sum;
+	// }
 
-		return sum;
+	public static long calcValidString(int n){
+		if (n == 0) {
+			return 0;
+		}
+		if (n == 1) {
+			return 2;
+		}
+		long[][] dp = new long[2][n];
+		// dp[0][n] represent number of possible strings of length n ending with
+		// a 0
+		// dp[1][n] represent number of possible strings of length n ending with
+		// a 1
+
+		dp[0][0] = 1;
+		dp[1][0] = 1;
+
+		for (int i = 1; i < n; i++) {
+			dp[0][i] = dp[1][i - 1];
+			dp[1][i] = dp[0][i - 1] + dp[1][i - 1];
+		}
+		return dp[0][n - 1] + dp[1][n - 1];
 	}
 
 	public static void main(String args[]){
@@ -51,7 +74,8 @@ public class ByteLand{
 		}
 
 		for(int i=0; i<test; i++){
-			System.out.println(calcValidString(n[i], false));
+			// System.out.println(calcValidString(n[i], false));
+			System.out.println(calcValidString(n[i]));
 		}
 	}
 }
